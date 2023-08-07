d514ccc4770daebd533d6fb34122fe5a368008ad
MHfoudari <eng.mohammed.alfoudari@gmail.com>
2023-08-07 23:28:25 +0300

    Created Amenities list

---
+++ src/apis/AmenityRating/index.js
@@ -0,0 +1,11 @@
+// const { instance } = require("..");
+
+// exports.getAllAmenityRating = async () => {
+//   const res = await instance.get("/amenityRating/");
+//   return res.data.reverse();
+// };
+
+// exports.getAmenityRatingById = async (id) => {
+//   const res = await instance.get(`/amenityRating/${id}`);
+//   return res.data;
+// };
