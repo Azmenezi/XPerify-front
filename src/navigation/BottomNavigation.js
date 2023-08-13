866223237f05fea4f717645dc186ca295bdb9f2b
MHfoudari <eng.mohammed.alfoudari@gmail.com>
2023-08-13 20:41:37 +0300

    Add an effect to the BottomNavBar

--- src/navigation/BottomNavigation.js
+++ src/navigation/BottomNavigation.js
@@ -1,15 +1,15 @@
+import React, { useRef, useContext } from "react";
+import { Animated, Image, View, TouchableOpacity } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome5, Ionicons } from "@expo/vector-icons";
+
 import CheckIn from "../screens/CheckIn/CheckIn";
 import MeetUp from "../screens/MeetUp/MeetUp";
 import Notification from "../screens/Notification/Notification";
-import { FontAwesome5 } from "@expo/vector-icons";
-import { Ionicons } from "@expo/vector-icons";
 import ROUTES from ".";
 import ProfileStackNavigstion from "./ProfileStackNavigstion";
 import PlaceStackNavigation from "./PlaceStackNavigation";
-import { useContext } from "react";
 import UserContext from "../context/UserContext";
-import { Image, View } from "react-native";
 import { BASE_URL } from "../apis";
 import CheckinStack from "./CheckinStack";
 
@@ -18,20 +18,77 @@ const Tab = createBottomTabNavigator();
 function BottomNavigation() {
   const { user } = useContext(UserContext);
 
+  const homeScale = useRef(new Animated.Value(1)).current;
+  const meetupScale = useRef(new Animated.Value(1)).current;
+  const checkinScale = useRef(new Animated.Value(1)).current;
+  const notificationScale = useRef(new Animated.Value(1)).current;
+  const profileScale = useRef(new Animated.Value(1)).current;
+
+  const handleTabPress = (animation) => {
+    Animated.sequence([
+      Animated.timing(animation, {
+        toValue: 0.95,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+      Animated.spring(animation, {
+        toValue: 1,
+        friction: 4,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
+
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerShown: false,
         tabBarActiveTintColor: "#5BA199",
         tabBarInactiveTintColor: "#E5E3E4",
-
         tabBarShowLabel: false,
         tabBarStyle: {
           backgroundColor: "#141519",
           borderTopColor: "#E5E3E420",
           borderTopWidth: 0.2,
         },
-      }}
+        tabBarButton: (props) => {
+          let animation;
+
+          switch (route.name) {
+            case ROUTES.HEDERROUTES.PLACE_STACK.STACK:
+              animation = homeScale;
+              break;
+            case ROUTES.HEDERROUTES.MEETUP:
+              animation = meetupScale;
+              break;
+            case ROUTES.HEDERROUTES.CHECKIN_STACK.STACK:
+              animation = checkinScale;
+              break;
+            case ROUTES.HEDERROUTES.NOTIFICATION:
+              animation = notificationScale;
+              break;
+            case ROUTES.HEDERROUTES.PROFILE_STACK.STACK:
+              animation = profileScale;
+              break;
+            default:
+              animation = new Animated.Value(1);
+          }
+
+          return (
+            <Animated.View
+              style={{ flex: 1, transform: [{ scale: animation }] }}
+            >
+              <TouchableOpacity
+                {...props}
+                onPress={() => {
+                  handleTabPress(animation);
+                  props.onPress();
+                }}
+              />
+            </Animated.View>
+          );
+        },
+      })}
     >
       <Tab.Screen
         name={ROUTES.HEDERROUTES.PLACE_STACK.STACK}
@@ -78,7 +135,6 @@ function BottomNavigation() {
         component={ProfileStackNavigstion}
         options={{
           tabBarIcon: ({ size, color }) => (
-            // <Ionicons name="person" size={24} color={color} />
             <View
               style={{
                 borderRadius: 50,
